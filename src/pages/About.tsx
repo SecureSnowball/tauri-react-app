d992219122b2cd3ba07574fa351802253a6d7a86
Captain Snowball <22148496+SecureSnowball@users.noreply.github.com>
2022-10-02 16:28:10 +0530

    Component

--- src/pages/About.tsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-    return (
-        <div>
-            <h1>Best ways to commit soocide</h1>
-        </div>
-    )
-}
-
-export default About
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,187 @@
-import { CssVarsProvider } from "@mui/joy";
-import Typography from "@mui/joy/Typography";
+import * as React from 'react';
+import { GlobalStyles } from '@mui/system';
+import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+import type { Theme } from '@mui/joy/styles';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import TextField from '@mui/joy/TextField';
+import IconButton from '@mui/joy/IconButton';
 
-const Dashboard = () => {
+// Icons import
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
+import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
+import MailRoundedIcon from '@mui/icons-material/MailRounded';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import MenuIcon from '@mui/icons-material/Menu';
+import BookRoundedIcon from '@mui/icons-material/BookRounded';
+
+// custom
+import emailTheme from '../theme';
+import Menu from '../components/Menu';
+import Layout from '../components/Layout';
+import Navigation from '../components/Navigation';
+import Mails from '../components/Mails';
+import EmailContent from '../components/EmailContent';
+
+const ColorSchemeToggle = () => {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="primary" />;
+  }
   return (
-    <CssVarsProvider>
-      <Typography level="h1" component="h2">
-        Hi, this is dashboard.
-      </Typography>
-    </CssVarsProvider>
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="outlined"
+      color="primary"
+      onClick={() => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+      }}
+    >
+      {mode === 'light' ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
   );
 };
 
-export default Dashboard;
+export default function EmailExample() {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  return (
+    <CssVarsProvider disableTransitionOnChange theme={emailTheme}>
+      <GlobalStyles<Theme>
+        styles={(theme) => ({
+          body: {
+            margin: 0,
+            fontFamily: theme.vars.fontFamily.body,
+          },
+        })}
+      />
+      {drawerOpen && (
+        <Layout.SideDrawer onClose={() => setDrawerOpen(false)}>
+          <Navigation />
+        </Layout.SideDrawer>
+      )}
+      <Layout.Root
+        sx={{
+          ...(drawerOpen && {
+            height: '100vh',
+            overflow: 'hidden',
+          }),
+        }}
+      >
+        <Layout.Header>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 1.5,
+            }}
+          >
+            <IconButton
+              variant="outlined"
+              size="sm"
+              onClick={() => setDrawerOpen(true)}
+              sx={{ display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <IconButton
+              size="sm"
+              variant="solid"
+              sx={{ display: { xs: 'none', sm: 'inline-flex' } }}
+            >
+              <MailRoundedIcon />
+            </IconButton>
+            <Typography component="h1" fontWeight="xl">
+              Email
+            </Typography>
+          </Box>
+          <TextField
+            size="sm"
+            placeholder="Search anything…"
+            startDecorator={<SearchRoundedIcon color="primary" />}
+            endDecorator={
+              <IconButton variant="outlined" size="sm" color="neutral">
+                <Typography fontWeight="lg" fontSize="sm" textColor="text.tertiary">
+                  /
+                </Typography>
+              </IconButton>
+            }
+            sx={{
+              flexBasis: '500px',
+              display: {
+                xs: 'none',
+                sm: 'flex',
+              },
+            }}
+          />
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
+            <IconButton
+              size="sm"
+              variant="outlined"
+              color="primary"
+              sx={{ display: { xs: 'inline-flex', sm: 'none' } }}
+            >
+              <SearchRoundedIcon />
+            </IconButton>
+            <IconButton
+              size="sm"
+              variant="outlined"
+              color="primary"
+              component="a"
+              href="/blog/first-look-at-joy/"
+            >
+              <BookRoundedIcon />
+            </IconButton>
+            <Menu
+              id="app-selector"
+              control={
+                <IconButton
+                  size="sm"
+                  variant="outlined"
+                  color="primary"
+                  aria-label="Apps"
+                >
+                  <GridViewRoundedIcon />
+                </IconButton>
+              }
+              menus={[
+                {
+                  label: 'Email',
+                  active: true,
+                  href: '/joy-ui/getting-started/templates/email/',
+                  'aria-current': 'page',
+                },
+                {
+                  label: 'Team',
+                  href: '/joy-ui/getting-started/templates/team/',
+                },
+                {
+                  label: 'Files',
+                  href: '/joy-ui/getting-started/templates/files/',
+                },
+              ]}
+            />
+            <ColorSchemeToggle />
+          </Box>
+        </Layout.Header>
+        <Layout.SideNav>
+          <Navigation />
+        </Layout.SideNav>
+        <Layout.Main>
+          <EmailContent />
+        </Layout.Main>
+      </Layout.Root>
+    </CssVarsProvider>
+  );
+}
